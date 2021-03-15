0287273566ce339b1eeb1eb9fe7e6f3c9975ed67
Brett Bloethner <brett@wirehead.io>
2021-03-15 15:58:42 -0600

    refactor the masync mult requesst loops for drug images and spl history

--- src/models/Drug/DrugBuilder.ts
+++ src/models/Drug/DrugBuilder.ts
@@ -20,107 +20,108 @@ interface DrugBuilderInterface {
  */
 @singleton()
 export class DrugBuilder implements DrugBuilderInterface {
-    openFDAService = container.resolve(OpenFDAService);
-    dailyMedService = container.resolve(DailyMedService);
-    RxImageService = container.resolve(RxImageService);
-    drug: Drug = new Drug();
+  openFDAService = container.resolve(OpenFDAService);
+  dailyMedService = container.resolve(DailyMedService);
+  RxImageService = container.resolve(RxImageService);
+  drug: Drug = new Drug();
 
-    constructor() {
-      this.reset();
+  constructor() {
+    this.reset();
+  }
+
+  /**
+   * reset
+   * TODO: Add Comments
+   */
+  public reset(): void {
+    this.drug = new Drug();
+  }
+
+  /**
+   * setDrugApplicationNumber
+   * TODO: Add Comments
+   * @param applicationNumber
+   */
+  public setDrugApplicationNumber(applicationNumber: string): void {
+    this.drug.setApplicationNumber(applicationNumber);
+  }
+
+  /**
+   * buildDrugMetadata
+   * TODO: Add Comments
+   */
+  public async buildDrugMetadata(): Promise<void> {
+    const drugMetadata = await this.openFDAService.getDrugByApplicationID(this.drug.getApplicationNumber());
+    this.drug.setDrugMetadata(drugMetadata);
+  }
+
+  /**
+   * buildDrugSPLHistory
+   * TODO: Add Comments
+   */
+  public async buildDrugSPLHistory() {
+    const drugSPLSetIds = this.drug.metadata!.splSetId;
+    const drugSplHistories: DrugSplLabelHistory[] = [];
+
+    const splHistoryRequests: Promise<void>[] = [];
+    for (let x = 0; x < drugSPLSetIds?.length; x++) {
+      splHistoryRequests.push(
+        this.dailyMedService.getSplHistory(drugSPLSetIds[x])
+          .then((history: DailyMedSplLabelHistory) => {
+            drugSplHistories.push({
+              spl_set_id: history.spl.setid,
+              title: history.spl.title,
+              history: history.history,
+            });
+          }),
+      );
     }
 
-    /**
-     * reset
-     * TODO: Add Comments
-     */
-    public reset(): void {
-      this.drug = new Drug();
+    await Promise.all(splHistoryRequests);
+    this.drug.setDrugSplHistories(drugSplHistories);
+  }
+
+  /**
+   * buildDrugImages
+   * TODO: Add Comments
+   */
+  public async buildDrugImages() {
+    const drugSPLSetIds = this.drug.metadata!.splSetId;
+    const drugImages: string[] = [];
+
+    const drugImagesRequests: Promise<any>[] = [];
+    for (let x = 0; x < drugSPLSetIds?.length; x++) {
+      drugImagesRequests.push(
+        this.RxImageService.getDrugImagesBySplSetId(drugSPLSetIds[x])
+          .then((imageMetadatas: any) => {
+            imageMetadatas.forEach((imageMetadata: any) => {
+              drugImages.push(imageMetadata.imageUrl);
+            });
+          }),
+      );
     }
 
-    /**
-     * setDrugApplicationNumber
-     * TODO: Add Comments
-     * @param applicationNumber
-     */
-    public setDrugApplicationNumber(applicationNumber: string): void {
-      this.drug.setApplicationNumber(applicationNumber);
-    }
+    await Promise.all(drugImagesRequests);
+    this.drug.setDrugImages(drugImages);
+  }
 
-    /**
-     * buildDrugMetadata
-     * TODO: Add Comments
-     */
-    public async buildDrugMetadata(): Promise<void> {
-      const drugMetadata = await this.openFDAService.getDrugByApplicationID(this.drug.getApplicationNumber());
-      this.drug.setDrugMetadata(drugMetadata);
-    }
+  /**
+   * buildDrugSPLs
+   * TODO: Add Comments
+   */
+  public async buildDrugSPLs(): Promise<void> {
+    const drugSpls = await this.openFDAService.getLabelsByApplicationID(this.drug.getApplicationNumber());
+    this.drug.setDrugSPLs(drugSpls);
+  }
 
-    /**
-     * buildDrugSPLHistory
-     * TODO: Add Comments
-     */
-    public async buildDrugSPLHistory() {
-      const drugSPLSetIds = this.drug.metadata!.splSetId;
-      const drugSplHistories: DrugSplLabelHistory[] = [];
-
-      const splHistoryRequests: Promise<DailyMedSplLabelHistory>[] = [];
-      for (let x = 0; x < drugSPLSetIds?.length; x++) {
-        splHistoryRequests.push(this.dailyMedService.getSplHistory(drugSPLSetIds[x]));
-      }
-
-      const histories = await Promise.all(splHistoryRequests);
-
-      histories.forEach((history: DailyMedSplLabelHistory) => {
-        drugSplHistories.push({
-          spl_set_id: history.spl.setid,
-          title: history.spl.title,
-          history: history.history,
-        });
-      });
-
-      this.drug.setDrugSplHistories(drugSplHistories);
-    }
-
-    /**
-     * buildDrugImages
-     * TODO: Add Comments
-     */
-    public async buildDrugImages() {
-      const drugSPLSetIds = this.drug.metadata!.splSetId;
-      const drugImages: string[] = [];
-
-      const drugImagesRequests: Promise<any>[] = [];
-      for (let x = 0; x < drugSPLSetIds?.length; x++) {
-        drugImagesRequests.push(this.RxImageService.getDrugImagesBySplSetId(drugSPLSetIds[x]));
-      }
-
-      const imageSets = await Promise.all(drugImagesRequests);
-      imageSets.forEach((imageMetadatas: any) => {
-        imageMetadatas.forEach((imageMetadata: any) => {
-          drugImages.push(imageMetadata.imageUrl);
-        });
-      });
-
-      this.drug.setDrugImages(drugImages);
-    }
-
-    /**
-     * buildDrugSPLs
-     * TODO: Add Comments
-     */
-    public async buildDrugSPLs(): Promise<void> {
-      const drugSpls = await this.openFDAService.getLabelsByApplicationID(this.drug.getApplicationNumber());
-      this.drug.setDrugSPLs(drugSpls);
-    }
-
-    /**
-     * getDrug
-     * TODO: Add Comments
-     * @returns
-     */
-    public getDrug(): Drug {
-      const result = this.drug;
-      this.reset();
-      return result;
-    }
+  /**
+   * getDrug
+   * TODO: Add Comments
+   * @returns
+   */
+  public getDrug(): Drug {
+    const result = this.drug;
+    this.reset();
+    return result;
+  }
 }
