b4b0ace788dbe1b3e82381adef53c01f84668853
Brett Bloethner <brett@wirehead.io>
2021-03-15 23:17:16 -0600

    add comments

--- src/controllers/drugs.controller.ts
+++ src/controllers/drugs.controller.ts
@@ -1,11 +1,18 @@
 /* eslint-disable max-len */
-// ./src/controllers/drugs.controller.ts
 import 'reflect-metadata';
 import { singleton } from 'tsyringe';
 import { Drug } from '../models/Drug/Drug.model';
 import { DrugDirector } from '../models/Drug/DrugDirector';
 import { OpenFDAService } from '../services/OpenFDA.service';
 import { OpenFDADrug } from '../services/OpenFDADrug.interface';
+
+/**
+ * Drugs Controller drugs.controller.ts
+ * This controller class is reponsible for organizing all the requests that come from routes
+ * beginning with /drugs. This primarily inlcudes the drugs search path and the get drug by
+ * FDA Application number path.
+ */
+
 @singleton()
 export class DrugsController {
     private drugDirector: DrugDirector;
@@ -20,12 +27,15 @@ export class DrugsController {
     }
 
     /**
-     * Find a drugs metadata by providing a search query and a search type.
-     * The search types are defined in the openFDA Service anre are brand_name, application_number,
-     * generic_name, and manufacturer_name.
+     * Find drugs by providing a search query and a search type. This will return a drug entry
+     * similar in structure to whats provided by the Open FDA search API. It does not return a
+     * JSON structure that adheres to this APIs Drug model and should be used primarily for
+     * finding a drugs FDA Application Number (NDA Number)
+     *
      * @param searchQuery String: the value for which a match or partial match is being sought
-     * @param searchType String: the drug parameter scope of the search query
-     * @returns OpenFDADrug[] a populated or empty array of OpenFDADrug that provide drug metadata
+     * @param searchType String: param on which the search will be conducted (brand_name,
+     * application_nmber, generic_name, or manufacturer_name)
+     * @returns OpenFDADrug[] a populated or empty array of OpenFDADrugs
      */
     public findDrug = async (searchQuery: string, searchType: string): Promise<OpenFDADrug[]> => {
       const results = await this.openFDAService.search(searchQuery, searchType);
@@ -33,8 +43,14 @@ export class DrugsController {
     }
 
     /**
-     * TODO: Add a comment here
-     * @param applicationNumber
+     * Get a Drugs data by the Drugs FDA Application Number. This will return, at a minimum the Drug
+     * model populated with the Drugs metadata. Boolean arguments are required to determine what
+     * additional data will be fetched and added to the Drug model return to the caller.
+     * @param applicationNumber String: FDA Application Number belonging to a drug in the Open FDA API/DB
+     * @param shouldGetSplHistory Boolean: Determines whether SPL History metadata will be fetched and added to the resp
+     * @param shouldGetCurrentSplLabel Boolean: Determines whether the drugs current SPL Label will be fetched and added to the resp
+     * @param shouldGetDrugImages Boolean: Determines whether the drugs image urls will be fetched and added to the resp
+     * @returns Drug: A purpose built Drug model containing Drug metadata and a variety of ther params based on req options
      */
     public getDrugByApplicationNumber = async (applicationNumber: string, shouldGetSplHistory: boolean, shouldGetCurrentSplLabel: boolean, shouldGetDrugImages: boolean): Promise<Drug> => {
       const drug = await this.drugDirector.buildExtensibleDrugWithMetadata(applicationNumber, shouldGetSplHistory, shouldGetCurrentSplLabel, shouldGetDrugImages);
--- src/models/Drug/Drug.model.ts
+++ src/models/Drug/Drug.model.ts
@@ -2,6 +2,14 @@ import { OpenFDADrug } from '../../services/OpenFDADrug.interface';
 import { DrugPlan } from './DrugPlan.interface';
 import { DrugMetadata, DrugSplLabelHistory } from './Drug.model.interfaces';
 
+/**
+ * Drug Model Drug.model.ts
+ * This Drug model is responsible for the structure of the custom Drug returned by this API
+ * on many of the Drug related calls this API supports. It is part of a builder pattern
+ * supported by DrugBuilder.ts, DrugDirector.ts and DrugPlan.interface.ts. As a result, it
+ * contains mostly setter methods and interface implementations.
+ */
+
 export class Drug implements DrugPlan {
     private applicationNumber: string| undefined;
     metadata: DrugMetadata | undefined;
@@ -9,10 +17,21 @@ export class Drug implements DrugPlan {
     drugImages: string[] | undefined;
     spls: any;
 
+    /**
+     * Set the application number of the Drug, this has to be done for all Drugs before
+     * data can be populated by the DrugBuilder.ts since all data is fetched from FDA
+     * APIs based on the Drugs FDA Application Number.
+     *
+     * @param applicationNumber String: A valid drug FDA Application Number
+     */
     public setApplicationNumber(applicationNumber: string): void {
       this.applicationNumber = applicationNumber;
     }
 
+    /**
+     * Get the application number of the Drug
+     * @returns String: applicationNumber param of this Drug if one exists, otherwise throw Error
+     */
     public getApplicationNumber(): string {
       if (!this.applicationNumber) {
         throw new Error('applicationNumber must be defined before building drug model.');
@@ -20,6 +39,12 @@ export class Drug implements DrugPlan {
       return this.applicationNumber;
     }
 
+    /**
+     * Load metadata into the Drug model based on the OpenFDADrug interface. The OpenFDADrug
+     * interface is based on an Open FDA API response body and so should facilitate easy
+     * movement of data from a OpenFDA API response to the metadata of a Drug object.
+     * @param metadata OpenFDADrug: drug inforamtion object fetched from the OpenFDA Drug DB API
+     */
     public setDrugMetadata(metadata: OpenFDADrug): void {
       this.metadata = {
         applicationNumber: metadata.application_number,
@@ -35,14 +60,36 @@ export class Drug implements DrugPlan {
       };
     }
 
+    /**
+     * Load drug SPL metadata history into the Drug model based on the DrugSplLabelHistory
+     * interface. The DrugSPLLabelHistory interface is based on an DailyMed response body
+     * and so should facilitate easy movement of data from a DailyMed API response to
+     * splHistories parameter of a Drug object.
+     * @param splHistories DrugSPLLabelHistory[]: Array of historical drug label metadata objects
+     * where each item in the array represents a drug label set (by the industry standard SPL
+     * Set Id)
+     */
     public setDrugSplHistories(splHistories: DrugSplLabelHistory[]) {
       this.drugSplHistory = splHistories;
     }
 
+    /**
+     * Load drug image URLs into the Drug model. Drug image urls are simple strings in an array
+     * where the string is a full URL path to an image of the drug. This method is mean to be
+     * used to load images into the Drug model from RxImage.service.ts
+     * @param drugImages String[]: Array of URL paths to images of the Drug
+     */
     public setDrugImages(drugImages: string[]) {
       this.drugImages = drugImages;
     }
 
+    /**
+     * Load drug SPL object into the Drug model. The Drug SPL object is a large and somewhat
+     * dynamically typed object that may or may not contain all the paramters available via
+     * the SPL (Structure Product Language) standard used by the FDA.
+     * @param spls: any: An SPL label object where the different params represent parsed out
+     * sections of the drugs label
+     */
     public setDrugSPLs(spls: any): void {
       this.spls = spls;
     }
