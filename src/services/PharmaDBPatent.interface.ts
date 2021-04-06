69e08ddc8fbcfff90025e0a0e1eb0be964ed0a1f
Brett Bloethner <brett@bl8nr.com>
2021-04-06 17:05:20 -0600

    add more realistic mock patent data

--- src/services/PharmaDBPatent.interface.ts
+++ src/services/PharmaDBPatent.interface.ts
@@ -1,16 +1,18 @@
 export interface PharmaDBPatentInterface {
     patent_number: string;
-    assignee: {
-        org_name: string
+    assignee?: {
+        org_name: string;
     }[];
-    inventors: {
-        first_name: string,
-        last_name: string
+    inventors?: {
+        first_name: string;
+        last_name: string;
     }[];
     claims: {
-        claim_number: string,
-        claim_text: string,
+        claim_number: number;
+        claim_text: string;
+        dependencies: string | null;
     }[];
-    submission_date: string;
+    submission_date?: string;
     published_date: string;
+    expiration_date: string;
 }
