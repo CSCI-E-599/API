c8934aaa372d52f122c768f01dfc1c0b6befd64a
Brett Bloethner <brett@bl8nr.com>
2021-04-25 13:56:29 -0600

    replace the mock data for the labels

--- src/services/PharmaDBLabel.interface.ts
+++ src/services/PharmaDBLabel.interface.ts
@@ -1,19 +1,59 @@
 export interface PharmaDBLabelInterface {
-    application_numbers: string[];
-    set_id: string;
-    spl_id: string;
-    spl_version: string;
-    sections: {
-        name: string;
-        text: string;
-        scores?: {
-            patentNumber: string;
-            claimNumber: number;
-            parentClaimNumbers: number[];
-            score: number;
-        }[];
-    }[];
-    published_date: string;
+    '_id': {
+        '$oid': string
+    },
+    'application_numbers': string[],
+    'set_id': string,
+    'spl_id': string,
+    'spl_version': string,
+    'published_date': string,
+    'sections': {
+        'name': string,
+        'text': string,
+        'parent': string
+    }[],
+    'previous_label_published_date': string,
+    'previous_label_spl_id': string,
+    'previous_label_spl_version': string,
+    'next_label_published_date': string,
+    'next_label_spl_id': string,
+    'next_label_spl_version': string,
+    'diff_against_previous_label': {
+        'name': string,
+        'text': any[][],
+        'parent': string,
+        'additions':
+            {
+                'indices': number[],
+                'expanded_context': string,
+                'scores':
+                    {
+                        'patent_number': string,
+                        'claim_number': number,
+                        'parent_claim_numbers': number[]
+                        'score': number
+                    }[]
+            }[]
+    }[]
 }
+
+// OLD INTERFACE
+// export interface PharmaDBLabelInterface {
+//     application_numbers: string[];
+//     set_id: string;
+//     spl_id: string;
+//     spl_version: string;
+//     sections: {
+//         name: string;
+//         text: string;
+//         scores?: {
+//             patentNumber: string;
+//             claimNumber: number;
+//             parentClaimNumbers: number[];
+//             score: number;
+//         }[];
+//     }[];
+//     published_date: string;
+// }
 
 // TODO: add an enum of the section names for the labels
